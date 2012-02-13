b50a4b3ed80d3864a846c63f365eb7a10dda4aa6
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-13 23:46:29 +0100

    try to remove lein-midje from :dev-dependencies (let's hope travis accept this)

--- project.clj
+++ project.clj
@@ -3,8 +3,7 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.3.2"]
                  [org.clojure/core.memoize "0.5.1"]]
-  :dev-dependencies [[midje "1.3.1"]
-                     [lein-midje "1.0.7"]]
+  :dev-dependencies [[midje "1.3.1"]]
   ;; Global exclusions to absolutely prevent deps-errors
   ;; Needed by Travis. See: http://about.travis-ci.org/docs/user/languages/clojure/
   :exclusions [org.clojure/clojure
