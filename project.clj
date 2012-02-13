362fcffddb9d2b489294ede0442fc5cdd530a0a7
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-13 23:33:02 +0100

    remove clj-tagsoup and data.json

--- project.clj
+++ project.clj
@@ -2,9 +2,6 @@
   :description "A simple library check if humanity has finally destroyed itself"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.3.2"]
-                 [org.clojure/data.json "0.1.1"]
-                 [clj-tagsoup "0.2.6" :exclusions [org.clojure/clojure
-                                                   swank-clojure/swank-clojure]]
                  [org.clojure/core.memoize "0.5.1"]]
   :dev-dependencies [[midje "1.3.1"]
                      [lein-midje "1.0.7"]])
