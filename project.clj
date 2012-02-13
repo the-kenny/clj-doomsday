cfac86269281748a2e346382d4ac1720c3d5b602
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-14 00:49:14 +0100

    fix additional parenthesis

--- project.clj
+++ project.clj
@@ -2,7 +2,8 @@
   :description "A simple library check if humanity has finally destroyed itself"
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [clj-http "0.3.2"]
-                 [org.clojure/core.memoize "0.5.1"]]
+                 [org.clojure/core.memoize "0.5.1"]
+                 [ring/ring-jetty-adapter "0.3.9"]]
   :dev-dependencies [[midje "1.3.1"]]
   ;; Global exclusions to absolutely prevent deps-errors
   ;; Needed by Travis. See: http://about.travis-ci.org/docs/user/languages/clojure/
