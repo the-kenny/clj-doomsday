83767336c3237571269252139bd64805f558a3c0
Moritz Ulrich <ulrich.moritz@googlemail.com>
2012-02-14 01:16:18 +0100

    cache the lhc

--- src/doomsday/lhc.clj
+++ src/doomsday/lhc.clj
@@ -1,12 +1,22 @@
 (ns doomsday.lhc
-  (:require [clj-http.client :as client]))
+  (:require [clj-http.client :as client]
+            [clojure.core.memoize :as memo]))
 
 (def lhc-url "http://hasthelargehadroncolliderdestroyedtheworldyet.com/")
 
 (defn ^:private extract-answer [s]
   (second (re-find #"<noscript>(.+)</noscript>" s)))
 
-(defn has-the-lhc-destroyed-the-world-yet? []
+(defn has-the-lhc-destroyed-the-world-yet?-uncached []
   (-> (:body (client/get lhc-url))
       (extract-answer)
       (not= "NOPE.")))
+
+(def ttl-milliseconds (* 1000           ;1 Second
+                         60             ;1 Minute
+                         60             ;1 Hour
+                         12             ;1/2 Day
+                         ))
+
+(def has-the-lhc-destroyed-the-world-yet?
+  (memo/memo-ttl has-the-lhc-destroyed-the-world-yet?-uncached ttl-milliseconds))
